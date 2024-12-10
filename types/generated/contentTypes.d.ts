80fc6fc7aa8c493f41621186e6fd3496f017eb0c
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-12-10 16:10:33 +0500

    Add Category field

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -447,6 +447,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
   };
   attributes: {
     available: Schema.Attribute.Boolean & Schema.Attribute.Required;
+    category: Schema.Attribute.String;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
